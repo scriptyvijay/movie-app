ce3079e62daab615b928170c923e978df9f8fdd7
Vijay Choudhary <scriptyvijay@gmail.com>
2022-06-27 23:51:20 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<input type="search" name="" id="" className="search" />
+			<button>Search</button>
+		</div>
+	);
+};
+
+export default Navbar;
